37aa2fdcd700e64f5c369b7ab5db258f3f08fc4d
Renato Dardengo <rdardengo01@gmail.com>
2023-07-09 00:43:42 -0300

    Ajustes

--- src/admin/components/Navbar/index.tsx
+++ src/admin/components/Navbar/index.tsx
@@ -1,18 +1,22 @@
-import MenuButton from "../../assets/img/menu.png"
-import "./style.css"
-export const Navbar = () => {
+import React from 'react';
+import MenuButton from '../../assets/img/menu.png';
+import './style.css';
+type NavbarProps = {
+    onCollapse: () => void;
+};
+export const Navbar = ({ onCollapse }: NavbarProps) => {
+    const handleCollapse = () => {
+        onCollapse();
+    };
+
     return (
         <div className="navbar">
             <div>
-                <img src={MenuButton} alt="" />
+                <img src={MenuButton} alt="" onClick={handleCollapse} />
             </div>
-            <div>
-                Usuário Logado
-            </div>
-            <div>
-                sair
-            </div>
-
+            <div>Usuário Logado</div>
+            <div>sair</div>
         </div>
     );
-}
+};
+
--- src/admin/pages/Home/index.tsx
+++ src/admin/pages/Home/index.tsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import { Sidebar } from "../../components/Sidebar";
-import { Main } from "../../components/Main";
-import { Navbar } from "../../components/Navbar";
-import "./style.css"
-export const Home=()=>{
-    return(
-        <div className='container'>
-            <Navbar/>
-            <Sidebar/>
-            <Main/>
+import React, { useState } from 'react';
+import { Sidebar } from '../../components/Sidebar';
+import { Main } from '../../components/Main';
+import { Navbar } from '../../components/Navbar';
+import './style.css';
+
+export const Home = () => {
+    const [collapse, setCollapse] = useState(false);
+
+    const handleCollapse = () => {
+        setCollapse(!collapse);
+    };
+
+    return (
+        <div className="container">
+            <Navbar onCollapse={handleCollapse} />
+            <div className={`sidebar ${collapse ? 'sidebar-collapsed' : ''}`}>
+                <Sidebar />
+            </div>
+            <Main />
         </div>
-
     );
+};
 
-}
